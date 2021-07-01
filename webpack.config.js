9a2fc56926c1d2960647286077deaeabd5804829
Jennifer Kirsch <jennifer.kirsch@binary-butterfly.de>
2021-07-01 13:40:32 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = (env, argv) => {
+    console.log(path.join(__dirname + '/dist'));
+    return {
+        target: 'web',
+        entry: './src/js/Calendar.js',
+        output: {
+            path: path.join(__dirname + '/dist'),
+            filename: 'binaryBookingCalendar.js',
+            library: 'BinaryBookingCalendar',
+            libraryTarget: 'umd',
+            globalObject: 'this',
+            umdNamedDefine: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    exclude: /node-modules/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+    };
+};
